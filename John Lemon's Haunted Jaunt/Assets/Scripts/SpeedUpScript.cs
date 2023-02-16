df4262feec4e075ecefbdd8613e9860a5887c220
iiaaannnn <212184x@mymail.nyp.edu.sg>
2023-02-16 14:56:01 +0800

    .

---
+++ John Lemon's Haunted Jaunt/Assets/Scripts/SpeedUpScript.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedUpScript : MonoBehaviour
+{
+    public float timeRemaining = 15; //timer for item respawn
+    public float powerupDuration = 5; //timer for duration of powerup when player pick it ups
+    bool runTimer = false;
+    public bool hasPowerUp = false;
+    public PlayerMovement playerMovement;
+
+    void Start()
+    {
+        transform.position = new Vector3(-3.0f, 6.0f, -3.0f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+            runTimer = true;
+            hasPowerUp = true;
+            playerMovement.speedModifier = 10;
+        }
+
+    }
+
+    void Update()
+    {
+
+        if (runTimer)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+            }
+            else if (timeRemaining <= 0)
+            {
+                transform.position = new Vector3(-3.0f, 6.0f, -3.0f);
+                runTimer = false;
+                timeRemaining = 15;
+            }
+
+            if (hasPowerUp)
+            {
+                if (powerupDuration > 0)
+                {
+                    powerupDuration -= Time.deltaTime;
+                }
+                else if (powerupDuration <= 0)
+                {
+                    hasPowerUp = false;
+                    playerMovement.speedModifier = 4;
+                    powerupDuration = 5;
+                }
+            }
+
+        }
+    }
+
+}
